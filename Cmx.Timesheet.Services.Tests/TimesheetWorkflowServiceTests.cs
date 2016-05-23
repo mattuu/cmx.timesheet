de5099b1137d92290d9140ee685f1769138a4a92
mattuu <maciej@uliasz.com>
2016-05-23 21:09:07 +0100

    fixing failing unit tests

--- Cmx.Timesheet.Services.Tests/TimesheetWorkflowServiceTests.cs
+++ Cmx.Timesheet.Services.Tests/TimesheetWorkflowServiceTests.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using Cmx.Timesheet.DomainModel;
-using J2BI.Holidays.ProductBrief.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Ploeh.AutoFixture;
 
 namespace Cmx.Timesheet.Services.Tests
 {
@@ -9,29 +10,32 @@ namespace Cmx.Timesheet.Services.Tests
     public class TimesheetWorkflowServiceTests
     {
         private TimesheetWorkflowService _service;
-        private InMemoryTimesheetStore _timesheetStore;
+        private Mock<ITimesheetStore> _timesheetStoreMock;
+        private Fixture _fixture;
 
         [TestInitialize]
         public void Init()
         {
-            _timesheetStore = new InMemoryTimesheetStore();
-            _service = new TimesheetWorkflowService(_timesheetStore);
+            _fixture = new Fixture();
+            _timesheetStoreMock = _fixture.Freeze<Mock<ITimesheetStore>>();
+            _service = new TimesheetWorkflowService(_timesheetStoreMock.Object);
         }
 
         [TestMethod]
-        public void SubmitTimesheetShouldChangeTimesheetStatusToSubmitted()
+        public void SubmitTimesheet_ShouldSet_TimesheetStatus_To_Submitted()
         {
-            CheckStatusAfterAction(1, id => _service.SubmitTimesheet(id), TimesheetStatus.Submitted);
+            _timesheetStoreMock.Setup(s => s.GetTimesheetById(It.IsAny<int>())).Returns<int>(id => new TimesheetModel {Id = id});
+            CheckStatusAfterAction(_fixture.Create<int>(), id => _service.SubmitTimesheet(id), TimesheetStatus.Submitted);
         }
 
         [TestMethod]
-        public void ApproveTimesheetShouldChangeTimesheetStatusToApproved()
+        public void ApproveTimesheet_ShouldSet_TimesheetStatus_To_Approved()
         {
             CheckStatusAfterAction(1, id => _service.ApproveTimesheet(id), TimesheetStatus.Approved);
         }
 
         [TestMethod]
-        public void RejectTimesheetShouldChangeTimesheetStatusToRejected()
+        public void RejectTimesheet_ShouldSet_TimesheetStatus_To_Rejected()
         {
             CheckStatusAfterAction(1, id => _service.RejectTimesheet(id), TimesheetStatus.Rejected);
         }
@@ -60,7 +64,7 @@ namespace Cmx.Timesheet.Services.Tests
         private void CheckStatusAfterAction(int id, Action<int> action, TimesheetStatus status)
         {
             action(id);
-            Assert.AreEqual(status, _timesheetStore.GetTimesheetById(id).Status);
+            Assert.AreEqual(status, _timesheetStoreMock.Object.GetTimesheetById(id).Status);
         }
     }
 }

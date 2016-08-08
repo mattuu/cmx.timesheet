fa0dbe9e09626d8507884a1f6676a513df96edbc
Matt Uliasz <matt.uliasz@jet2.com>
2016-08-08 12:39:36 +0100

    Progress on DAL

---
+++ src/Cmx.Timesheet.DataAccess/ITimesheetDataStore.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cmx.Timesheet.DomainModel;
+
+namespace Cmx.Timesheet.DataAccess
+{
+    public interface ITimesheetDataStore
+    {
+        IEnumerable<TimesheetModel> GetTimesheetsByUser(string username);
+
+
+    }
+}

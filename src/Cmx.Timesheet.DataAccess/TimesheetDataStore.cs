2504cfb366d804d62f04cef40b3e52fcf74b839c
Matt Uliasz <matt.uliasz@jet2.com>
2016-12-19 15:38:51 +0000

    Progress on MongoDb TimesheetDataStore

--- src/Cmx.Timesheet.DataAccess/TimesheetDataStore.cs
+++ src/Cmx.Timesheet.DataAccess/TimesheetDataStore.cs
@@ -1,17 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Cmx.Timesheet.DomainModel;
+using MongoDB.Driver;
 
 namespace Cmx.Timesheet.DataAccess
 {
     public class TimesheetDataStore : ITimesheetDataStore
     {
-        public Task<IEnumerable<TimesheetModel>> GetTimesheets()
+        internal const string TimesheetCollectionName = "timesheets";
+        private readonly IMongoDatabase _mongoDatabase;
+        private IMongoCollection<TimesheetModel> _collection;
+
+        public TimesheetDataStore(IMongoDatabase mongoDatabase)
         {
-            throw new NotImplementedException();
+            if (mongoDatabase == null) throw new ArgumentNullException(nameof(mongoDatabase));
+            _mongoDatabase = mongoDatabase;
+
+            _collection = mongoDatabase.GetCollection<TimesheetModel>(TimesheetCollectionName, new MongoCollectionSettings
+            {
+                AssignIdOnInsert = true
+            });
+        }
+
+        public async Task<IEnumerable<TimesheetModel>> GetTimesheets()
+        {
+            return await _mongoDatabase.GetCollection<TimesheetModel>(TimesheetCollectionName)
+                                       .FindSync(tm => true)
+                                       .ToListAsync();
         }
 
         public IEnumerable<TimesheetModel> GetTimesheetsByUser(int ownerId)
@@ -19,9 +35,11 @@ namespace Cmx.Timesheet.DataAccess
             throw new NotImplementedException();
         }
 
-        public Task<TimesheetModel> GetTimesheetById(Guid timesheetId)
+        public async Task<TimesheetModel> GetTimesheetById(Guid timesheetId)
         {
-            throw new NotImplementedException();
+            return await _mongoDatabase.GetCollection<TimesheetModel>(TimesheetCollectionName)
+                                       .FindSync(tm => tm.Id == timesheetId)
+                                       .FirstAsync();
         }
 
         public Task<bool> UpdateTimesheet(TimesheetModel model)
--- test/Cmx.Timesheet.DataAccess.Tests/TimesheetDataStoreTests.cs
+++ test/Cmx.Timesheet.DataAccess.Tests/TimesheetDataStoreTests.cs
@@ -1,13 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Cmx.Timesheet.DomainModel;
 using Cmx.Timesheet.TestUtils.Attributes;
 using MongoDB.Driver;
+using Moq;
+using Ploeh.AutoFixture.Idioms;
+using Ploeh.AutoFixture.Xunit2;
 using Xunit;
 
 namespace Cmx.Timesheet.DataAccess.Tests
 {
     public class TimesheetDataStoreTests
     {
+        [Theory, AutoMoqData]
+        public void Ctor_ShouldThrowWhenAnyDependencyIsNull(GuardClauseAssertion assertion)
+        {
+            assertion.Verify(typeof(TimesheetDataStore).GetConstructors());
+        }
+
+        [Theory, AutoMoqData]
+        public async Task GetTimesheets_ShouldCall_GetCollection_On_MongoDatabase_WithCorrectArgs([Frozen] Mock<IMongoDatabase> mongoDatabaseMock, TimesheetDataStore sut)
+        {
+            // act..
+            await sut.GetTimesheets();
+
+            // assert..
+            mongoDatabaseMock.Verify(m => m.GetCollection<TimesheetModel>(It.Is<string>(s => s == TimesheetDataStore.TimesheetCollectionName), It.Is<MongoCollectionSettings>(s => s == null)), Times.Once());
+        }
+
+
+        [Theory(Skip = "TODO: come up with good way of testing this.."), AutoMoqData]
+        public async Task GetTimesheets_ShouldReturnCorrectResult([Frozen] Mock<IMongoDatabase> mongoDatabaseMock,
+                                                                  [Frozen] Mock<IMongoCollection<TimesheetModel>> mongoCollectionMock,
+                                                                  MongoCollectionSettings settings,
+                                                                  IEnumerable<TimesheetModel> expectedTimesheetModels,
+                                                                  TimesheetDataStore sut)
+        {
+            // arrange..
+            mongoDatabaseMock.Setup(m => m.GetCollection<TimesheetModel>(TimesheetDataStore.TimesheetCollectionName, settings)).Returns(mongoCollectionMock.Object);
+
+            // act..
+            var actual = await sut.GetTimesheets();
+
+            // assert..
+            mongoDatabaseMock.Verify(m => m.GetCollection<TimesheetModel>(It.Is<string>(s => s == TimesheetDataStore.TimesheetCollectionName), It.Is<MongoCollectionSettings>(s => s == null)), Times.Once());
+        }
+
+
         [Theory, AutoMoqData]
         public void GetTimesheets_ShouldReturnCorrectResult()
         {

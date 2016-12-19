bb47c6f771d145aae1da7695d79f919e236b2bfe
Matt Uliasz <matt.uliasz@jet2.com>
2016-12-19 11:22:56 +0000

    Added mongodb as data store

---
+++ tests/Cmx.Timesheet.TestUtils/AutoFixtureCustomizations/MapperCustomization.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using AutoMapper;
+using Ploeh.AutoFixture.Kernel;
+
+namespace Cmx.Timesheet.TestUtils.AutoFixtureCustomizations
+{
+    public class MapperCustomization : ISpecimenBuilder
+    {
+        private readonly IMapper _mapper;
+
+        public MapperCustomization(IMapper mapper)
+        {
+            if (mapper == null) throw new ArgumentNullException(nameof(mapper));
+            _mapper = mapper;
+        }
+
+        public object Create(object request, ISpecimenContext context)
+        {
+            var pi = request as ParameterInfo;
+            if (pi == null || pi.ParameterType != typeof(IMapper))
+            {
+                return new NoSpecimen();
+            }
+
+            return _mapper;
+        }
+    }
+}
---
+++ tests/Cmx.Timesheet.TestUtils/AutoFixtureCustomizations/RazorEngineServiceCustomization.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using Ploeh.AutoFixture.Kernel;
+using RazorEngine.Configuration;
+using RazorEngine.Templating;
+
+namespace Cmx.Timesheet.TestUtils.AutoFixtureCustomizations
+{
+    public class RazorEngineServiceCustomization : ISpecimenBuilder
+    {
+        private readonly Type _type;
+
+        public RazorEngineServiceCustomization(Type type)
+        {
+            if (type == null) throw new ArgumentNullException(nameof(type));
+            _type = type;
+        }
+
+        public object Create(object request, ISpecimenContext context)
+        {
+            var pi = request as ParameterInfo;
+            if (pi == null || pi.ParameterType != typeof(IRazorEngineService))
+            {
+                return new NoSpecimen();
+            }
+
+            return RazorEngineService.Create(new TemplateServiceConfiguration
+            {
+                TemplateManager = new EmbeddedResourceTemplateManager(_type)
+            });
+        }
+    }
+}
